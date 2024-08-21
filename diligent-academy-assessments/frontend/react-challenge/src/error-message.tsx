c602983dd388284859301f6a2046121aba11871a
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2024-08-21 11:30:56 +0200

    Style the error message

--- diligent-academy-assessments/frontend/react-challenge/src/error-message.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/error-message.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './styles.css';
 
 interface ErrorMessageProps {
     message: string;
 }
 
-const ErrorMessage: React.FC<ErrorMessageProps> = ({message}) => {
-    return <p style={{color: 'red'}}>{message}</p>;
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
+    return <p className="message error">{message}</p>;
 };
 
 export default ErrorMessage;
