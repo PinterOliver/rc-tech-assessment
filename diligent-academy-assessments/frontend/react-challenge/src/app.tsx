b4eecc7633563b23b9bee079f2d22e613ef96413
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2024-08-21 11:30:43 +0200

    Style the website

--- diligent-academy-assessments/frontend/react-challenge/src/app.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/app.tsx
@@ -2,12 +2,12 @@ import HeroesList from "./hero-list/heroes-list";
 
 function App() {
 
-  return (
-    <div>
-      <h1>App</h1>
-      <HeroesList />
-    </div>
-  );
+    return (
+        <div id="main">
+            <h1>App</h1>
+            <HeroesList/>
+        </div>
+    );
 }
 
 export default App;
